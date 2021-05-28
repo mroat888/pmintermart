ab9bd233eb547098fd8a6a2c47180689ff4a39d3
Issaranuwat <mroat07@gmail.com>
2021-05-28 21:41:08 +0700

    insert wordpress

--- config/config.php
+++ config/config.php
@@ -1,41 +1,41 @@
 <?php
 
-    define("DB_TYPE", "mysql");
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "enrhote_pmintermart");
-    define("DB_USER", "enrhote_pmintermart");
-    define("DB_PASS", "tlAv1HWP3");
-    define("URL","https://www.pmintermart.com/");
-    define("URL_IMG","https://www.pmintermart.com/");
-    define("FOLDER_PRODUCT","img_product");
-    define("FOLDER_SKU","img_product_sku");
-
-    define("DB_TYPE_WP", "mysql");
-    define("DB_HOST_WP", "localhost");
-    define("DB_NAME_WP", "enrhote_pmcontent");
-    define("DB_USER_WP", "enrhote_pmcontent");
-    define("DB_PASS_WP", "4wp0iW7L81");
-    define("URL_WP","https://www.pmintermart.com/pm-wpcontent/");
-    define("URL_IMG_WP","https://www.pmintermart.com/pm-wpcontent/");
-    
-
     // define("DB_TYPE", "mysql");
     // define("DB_HOST", "localhost");
     // define("DB_NAME", "enrhote_pmintermart");
-    // define("DB_USER", "root");
-    // define("DB_PASS", "");
-    // define("URL","http://127.0.0.1:8181/pmintermart/");
-    // define("URL_IMG","http://127.0.0.1:8181/pmintermart/");
+    // define("DB_USER", "enrhote_pmintermart");
+    // define("DB_PASS", "tlAv1HWP3");
+    // define("URL","https://www.pmintermart.com/");
+    // define("URL_IMG","https://www.pmintermart.com/");
     // define("FOLDER_PRODUCT","img_product");
     // define("FOLDER_SKU","img_product_sku");
-
 
     // define("DB_TYPE_WP", "mysql");
     // define("DB_HOST_WP", "localhost");
     // define("DB_NAME_WP", "enrhote_pmcontent");
-    // define("DB_USER_WP", "root");
-    // define("DB_PASS_WP", "");
-    // define("URL_WP","http://127.0.0.1:8181/pmintermart/wp-content/");
-    // define("URL_IMG_WP","http://127.0.0.1:8181/pmintermart/wp-content/");
+    // define("DB_USER_WP", "enrhote_pmcontent");
+    // define("DB_PASS_WP", "4wp0iW7L81");
+    // define("URL_WP","https://www.pmintermart.com/pm-wpcontent/");
+    // define("URL_IMG_WP","https://www.pmintermart.com/pm-wpcontent/");
+    
+
+    define("DB_TYPE", "mysql");
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "enrhote_pmintermart");
+    define("DB_USER", "root");
+    define("DB_PASS", "");
+    define("URL","http://127.0.0.1:8181/pmintermart/");
+    define("URL_IMG","http://127.0.0.1:8181/pmintermart/");
+    define("FOLDER_PRODUCT","img_product");
+    define("FOLDER_SKU","img_product_sku");
+
+
+    define("DB_TYPE_WP", "mysql");
+    define("DB_HOST_WP", "localhost");
+    define("DB_NAME_WP", "enrhote_pmcontent");
+    define("DB_USER_WP", "root");
+    define("DB_PASS_WP", "");
+    define("URL_WP","http://127.0.0.1:8181/pmintermart/wp-content/");
+    define("URL_IMG_WP","http://127.0.0.1:8181/pmintermart/wp-content/");
 
 ?>
