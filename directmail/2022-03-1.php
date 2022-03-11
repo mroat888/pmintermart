e6f93b7dddadafeb7b9c08aeb42c823c5be209ca
mroat888 <mroat07@gmail.com>
2022-03-11 08:57:38 +0700

    update directmail 2022-03

---
+++ directmail/2022-03-1.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🔥 ไอเทมฮอต.! รถเข็นแม่บ้าน รถเข็นบริการ รถเข็นผ้า</title>
+</head>
+<body>
+<style type="text/css">
+		body{
+			margin:0px;
+			padding:0px;
+			font-size:16px;
+			font-family: 'Arial';
+			color:#313131;
+			background-color:#f2f2f2
+		}
+
+		a{
+			text-decoration: none;
+		}
+
+		.container {
+		  padding-right: 15px;
+		  padding-left: 15px;
+		  margin-right: auto;
+		  margin-left: auto;
+		  width:650px;
+		}
+
+		.row {
+		  margin-right: -15px;
+		  margin-left: -15px;
+		}
+
+		.my-row{
+			float: left;
+			width: 100%;
+		}
+
+		.btn {
+		  display: inline-block;
+		  padding: 6px 12px;
+		  margin-bottom: 0;
+		  font-size: 14px;
+		  font-weight: normal;
+		  line-height: 1.42857143;
+		  text-align: center;
+		  white-space: nowrap;
+		  vertical-align: middle;
+		  -ms-touch-action: manipulation;
+		      touch-action: manipulation;
+		  cursor: pointer;
+		  -webkit-user-select: none;
+		     -moz-user-select: none;
+		      -ms-user-select: none;
+		          user-select: none;
+		  background-image: none;
+		  border: 1px solid transparent;
+		  border-radius: 4px;
+		}
+
+	</style>
+
+    <?php
+        $url_base = "https://www.pmintermart.com/directmail/";
+        $path_image = $url_base."image/2022-03-1/";
+        $url_link_1 = "https://www.pmintermart.com/category/85/รถเข็นแม่บ้าน/1/";
+        $url_link_2 = "https://www.pmintermart.com//category/84/รถเข็นบริการ/1/";
+        $url_link_3 = "https://www.pmintermart.com/category/86/รถเข็นผ้า/1/";
+    ?>
+
+    <div class="container">
+		<div class="my-row" style="text-align:center; margin-top:10px; margin-bottom:10px;">
+			<span style="font-size:14px;">
+                If you experience problems viewing this email or accessing any of the links, please 
+                <a href="<?php echo $url_base; ?>2022-02-1.php">click here</a>
+			</span>
+		</div>
+		<div class="my-row" style="text-align:center;">
+			<span style="font-size:14px;">หากคุณไม่สามารถดูจดหมายข่าวด้านล่างได้กรุณา 
+			<a href="<?php echo $url_base; ?>2022-02-1.php">คลิกที่นี่</a>
+			</span>
+		</div>
+	</div>
+
+    <div class="container">
+		<div class="my-row" style="background-color:#2959c2; 
+		  margin-right: auto;
+		  margin-left: auto;
+		  width:100%; height:5px; margin-top:5px;">
+		</div>
+	</div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:5px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_1; ?>">
+                    <img src='<?php echo $path_image."directmail_1.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_1; ?>">
+                        <h3>รถเข็นแม่บ้าน</h3>
+                    </a>
+                    <p> ออกแบบรูปทรงพื้นที่วาง 3 ชั้น จัดแยกวางของใช้เป็นสัดส่วน 
+                        โครงสร้าง เหล็ก สเตนเลส ไม้ด้วยวัสดุชนิดแข็งแรง ทนทาน ลูกล้อขนาดใหญ่มีคุณภาพ รับน้ำหนัก รับแรงกระแทกได้ดี 
+                        ไม่เกิดเสียงดัง ประหยัดเวลาและค่าใช้จ่ายในการซ่อมบำรุง</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_2; ?>">   
+                    <img src='<?php echo $path_image."directmail_2.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_2; ?>"> 
+                        <h3>รถเข็นบริการ</h3>
+                    </a>
+                    <p>
+                        รถเข็นเสิร์ฟอาหาร รถเข็นเครื่องดื่ม รถเข็นไวน์ รถเข็นบาร์เหล้า รถเข็นเก็บจาน 
+                        ออกแบบรูปทรงสวยงาม พื้นที่ชั้นวางของได้สัดส่วน บริกรเข็นเสิร์ฟด้วยความคล่องตัว 
+                        ผลิตด้วยโครงสร้างเหล็ก สเตนเลส ไม้ วัสดุชนิดแข็งแรง ทนทาน เพื่อรองรับการใช้งานทุกวัน 
+                        โดยเฉพาะลูกล้อ คุณภาพอย่างดี ไม่เกิดเสียงดังรบกวนแขกระหว่างเข็นบริการ 
+                        ช่วยประหยัดเวลาและค่าใช้จ่ายในการซ่อมบำรุง
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_3; ?>"> 
+                    <img src='<?php echo $path_image."directmail_3.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_3; ?>"> 
+                        <h3>รถเข็นผ้า</h3>
+                    </a>
+                        <p>
+                            รถเข็นผ้าใช้แล้ว รถเก็บผ้า รถเข็นผ้าซักรีดแล้ว ออกแบบหลายรูปทรงและขนาด
+                            และรับน้ำหนักจำนวนผ้าได้มาก แข็งแรง คงทน ราวจับเข็นเคลื่อนย้ายสะดวก ตัวโครง 
+                            ผนังลูกกรง ผลิตด้วยวัสดุ สแตนเลสและถังพลาสติกหนา หรือถุงผ้าใบหนา 
+                            ลูกล้อคุณภาพอย่างดี พร้อมตัวกันกระแทกที่มุมของรถ
+                        </p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#2959c2; padding-top:0px; text-align:center; line-height:30px;">
+			<div class="my-row" style="padding-bottom:0px;">
+                <h2 style="color:#FFFFFF">Contact : 083-025-8992</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <table style="width:90%; margin-right: auto; margin-left: auto;">
+                    <tr>
+                        <td style="text-align:right;"><strong>website:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.pmintermart.com/">https://www.pmintermart.com/</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Lazada:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://bit.ly/3l0wbz2">https://bit.ly/3l0wbz2</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Shopee:</strong></td>
+                        <td style="text-align:left;" >
+                            <a href="https://shp.ee/zr9df3j">https://shp.ee/zr9df3j</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Line official:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://page.line.me/pmintermart">https://page.line.me/pmintermart</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Facebook:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.facebook.com/PM-INTERMART-106806074049943">
+                                https://www.facebook.com/PM-INTERMART-106806074049943</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Instragram:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.instagram.com/pmintermartofficial/">
+                                https://www.instagram.com/pmintermartofficial/
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
