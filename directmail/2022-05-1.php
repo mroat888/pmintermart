6a0addd62eca0ea859bed4a062814f1f1cdce295
mroat888 <mroat07@gmail.com>
2022-05-10 21:02:37 +0700

    update

---
+++ directmail/2022-05-1.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🔥 ฝนมาแล้ว เข้าสู่ 'ฤดูฝน'  ต้นเดือน พ.ค.</title>
+</head>
+<body>
+<style type="text/css">
+		body{
+			margin:0px;
+			padding:0px;
+			font-size:16px;
+			font-family: 'Arial';
+			color:#313131;
+			background-color:#f2f2f2
+		}
+
+		a{
+			text-decoration: none;
+		}
+
+		.container {
+		  padding-right: 15px;
+		  padding-left: 15px;
+		  margin-right: auto;
+		  margin-left: auto;
+		  width:650px;
+		}
+
+		.row {
+		  margin-right: -15px;
+		  margin-left: -15px;
+		}
+
+		.my-row{
+			float: left;
+			width: 100%;
+		}
+
+		.btn {
+		  display: inline-block;
+		  padding: 6px 12px;
+		  margin-bottom: 0;
+		  font-size: 14px;
+		  font-weight: normal;
+		  line-height: 1.42857143;
+		  text-align: center;
+		  white-space: nowrap;
+		  vertical-align: middle;
+		  -ms-touch-action: manipulation;
+		      touch-action: manipulation;
+		  cursor: pointer;
+		  -webkit-user-select: none;
+		     -moz-user-select: none;
+		      -ms-user-select: none;
+		          user-select: none;
+		  background-image: none;
+		  border: 1px solid transparent;
+		  border-radius: 4px;
+		}
+
+        .p_descrpition{
+            padding-left:20px;
+            padding-right:20px;
+        }
+
+	</style>
+
+    <?php
+        $url_base = "https://www.pmintermart.com/directmail/";
+        $path_image = $url_base."image/2022-05-1/";
+        $url_link_1 = "https://www.pmintermart.com/collection/21/ที่ใส่ร่ม%20ที่เก็บร่ม/1/";
+        $url_link_2 = "https://www.pmintermart.com/collection/26/เครื่องสวมถุงร่มเปียก/1/";
+        $url_link_3 = "https://www.pmintermart.com/collection/40/ป้ายเตือนความปลอดภัย/1/";
+        $url_link_4 = "https://www.pmintermart.com/collection/7/ไดร์เป่าผม/1/";
+    ?>
+
+    <div class="container">
+		<div class="my-row" style="text-align:center; margin-top:10px; margin-bottom:10px;">
+			<span style="font-size:14px;">
+                If you experience problems viewing this email or accessing any of the links, please 
+                <a href="<?php echo $url_base; ?>2022-02-1.php">click here</a>
+			</span>
+		</div>
+		<div class="my-row" style="text-align:center;">
+			<span style="font-size:14px;">หากคุณไม่สามารถดูจดหมายข่าวด้านล่างได้กรุณา 
+			<a href="<?php echo $url_base; ?>2022-02-1.php">คลิกที่นี่</a>
+			</span>
+		</div>
+	</div>
+
+    <div class="container">
+		<div class="my-row" style="background-color:#2959c2; 
+		  margin-right: auto;
+		  margin-left: auto;
+		  width:100%; height:5px; margin-top:5px;">
+		</div>
+	</div>
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:5px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_1; ?>">
+                        <h2>เตรียมอุปกรณ์ พร้อมเข้า 'ฤดูฝน'  ต้นเดือน พ.ค.</h2>
+                    </a>
+                        <h3>ส่วนต้อนรับหน้าอาคาร ทางเดิน เตรียมอุปกรณ์ พร้อม ? <br>
+                        ให้ผู้มาใช้บริการเริ่มแรกอย่างใส่ใจ เกิดความรู้สึกดีๆ <br>ประทับใจในการต้อนรับ หน้าฝนนี้</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:5px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_1; ?>">
+                    <img src='<?php echo $path_image."directmail_1.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_1; ?>">
+                        <h3>ที่ใส่ร่ม ที่เก็บร่ม</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        เฟอร์นิเจอร์ทรงกระบอก ทรงเหลี่ยมแบ่งช่องตระแกรงไว้ เสียบ เก็บ แขวน ร่มพับ ร่มด้ามยาวเปียกฝน ให้เป็นระเบียบเรียบร้อย 
+                        ส่วนต้อนรับหมดปัญหาน้ำนองพื้นเปียก เปอะเปื้อนทางเข้าออกหน้าห้างร้าน ภัตตาคาร โรงแรมโรงงาน และสถานที่ราชการ
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_2; ?>">   
+                    <img src='<?php echo $path_image."directmail_2.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_2; ?>"> 
+                        <h3>เครื่องสวมถุงร่มเปียก</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        อุปกรณ์เครื่องส่วมใส่ถุงพลาสติก ห่อร่มพับ ร่มยาวเปียกฝน จัดวางหน้าอาคารส่วนต้อนรับลูกค้า 
+                        พื้นไม่เปียกลื่นแฉะ ผู้มาใช้บริการเดินเข้าออกสบายใจ
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_3; ?>"> 
+                    <img src='<?php echo $path_image."directmail_3.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_3; ?>"> 
+                        <h3>ป้ายเตือนความปลอดภัย</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        ป้ายตั้งวางที่เป็นรูปสัญญลักษณ์ หรือ ข้อความสากลสื่อ เตือน ห้าม ป้องกัน 
+                        ระวังอุบัติเหตุอาจเกิดขึ้นได้กับผู้เข้ามาใกล้พื้นที่ หรือ เดินผ่าน อย่ามองข้ามความปลอดภัย
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <a href="<?php echo $url_link_4; ?>"> 
+                    <img src='<?php echo $path_image."directmail_4.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <div class="my-row" style="text-align:center;">
+                    <a href="<?php echo $url_link_4; ?>"> 
+                        <h3>ไดร์เป่าผม</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        ผลิตภัณฑ์โรงแรมคุณภาพ รูปทรงล้ำยุค มาตรฐาน JAWA เยรมันนี ประสิทธิภาพสูง เครื่องทำงานเมื่อกดปุ่ม
+                        และเครื่องตัดอัตโนมัติเมื่อความร้อนสูงเกิน เพิ่มความปลอดภัย 2เท่า ทั้งแบบแขวนติดผนังและพับเก็บ
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#2959c2; padding-top:0px; text-align:center; line-height:30px;">
+			<div class="my-row" style="padding-bottom:0px;">
+                <h2 style="color:#FFFFFF">Contact : 083-025-8992</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <table style="width:90%; margin-right: auto; margin-left: auto;">
+                    <tr>
+                        <td style="text-align:right;"><strong>website:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.pmintermart.com/">https://www.pmintermart.com/</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Lazada:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://bit.ly/3l0wbz2">https://bit.ly/3l0wbz2</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Shopee:</strong></td>
+                        <td style="text-align:left;" >
+                            <a href="https://shp.ee/zr9df3j">https://shp.ee/zr9df3j</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Line official:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://page.line.me/pmintermart">https://page.line.me/pmintermart</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Facebook:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.facebook.com/PM-INTERMART-106806074049943">
+                                https://www.facebook.com/PM-INTERMART-106806074049943</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Instragram:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.instagram.com/pmintermartofficial/">
+                                https://www.instagram.com/pmintermartofficial/
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
