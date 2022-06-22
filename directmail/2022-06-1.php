5a59823db10002e4f0bdd87ef67f2d5ed385a576
mroat888 <mroat07@gmail.com>
2022-06-22 09:27:51 +0700

    update directmail

---
+++ directmail/2022-06-1.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🔥 [แนะนำสินค้า] ที่กดสบู่ จานวางสบู่แก้ว แผ่นรองกันลื่น</title>
+</head>
+<body>
+<style type="text/css">
+		body{
+			margin:0px;
+			padding:0px;
+			font-size:16px;
+			font-family: 'Arial';
+			color:#313131;
+			background-color:#f2f2f2
+		}
+
+		a{
+			text-decoration: none;
+		}
+
+		.container {
+		  padding-right: 15px;
+		  padding-left: 15px;
+		  margin-right: auto;
+		  margin-left: auto;
+		  width:650px;
+		}
+
+		.row {
+		  margin-right: -15px;
+		  margin-left: -15px;
+		}
+
+		.my-row{
+			float: left;
+			width: 100%;
+		}
+
+		.btn {
+		  display: inline-block;
+		  padding: 6px 12px;
+		  margin-bottom: 0;
+		  font-size: 14px;
+		  font-weight: normal;
+		  line-height: 1.42857143;
+		  text-align: center;
+		  white-space: nowrap;
+		  vertical-align: middle;
+		  -ms-touch-action: manipulation;
+		      touch-action: manipulation;
+		  cursor: pointer;
+		  -webkit-user-select: none;
+		     -moz-user-select: none;
+		      -ms-user-select: none;
+		          user-select: none;
+		  background-image: none;
+		  border: 1px solid transparent;
+		  border-radius: 4px;
+		}
+
+        .p_descrpition{
+            padding-left:20px;
+            padding-right:20px;
+        }
+
+	</style>
+
+    <?php
+        $url_base = "https://www.pmintermart.com/directmail/";
+        $path_image = $url_base."image/2022-06-1/";
+        $url_link_1 = "https://www.pmintermart.com/collection/13/ที่กดสบู่/1/";
+        $url_link_2 = "https://www.pmintermart.com/category/89/แก้ว/1/";
+        $url_link_3 = "https://www.pmintermart.com/collection/16/แผ่นรองกันลื่น/1/";
+    ?>
+
+    <div class="container">
+		<div class="my-row" style="text-align:center; margin-top:10px; margin-bottom:10px;">
+			<span style="font-size:14px;">
+                If you experience problems viewing this email or accessing any of the links, please 
+                <a href="<?php echo $url_base; ?>2022-02-1.php">click here</a>
+			</span>
+		</div>
+		<div class="my-row" style="text-align:center;">
+			<span style="font-size:14px;">หากคุณไม่สามารถดูจดหมายข่าวด้านล่างได้กรุณา 
+			<a href="<?php echo $url_base; ?>2022-02-1.php">คลิกที่นี่</a>
+			</span>
+		</div>
+	</div>
+
+    <div class="container">
+		<div class="my-row" style="background-color:#2959c2; 
+		  margin-right: auto;
+		  margin-left: auto;
+		  width:100%; height:5px; margin-top:5px;">
+		</div>
+	</div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:5px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px; text-align:center;">
+                <a href="<?php echo $url_link_1; ?>">
+                    <img src='<?php echo $path_image."directmail_1.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <span>
+                    <a href="<?php echo $url_link_1; ?>">
+                        <h3>ที่กดสบู่ติดผนัง</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        ที่กดสบู่ล้างมือติดผนัง เป็นแบบจุกปั๊ม ผลิตด้วยวัสดุ อคิลิค พลาสติก ABS และสแตนเลส 
+                        รูปทรงทันสมัย ควบคุมปริมาณการกดได้แม่นยำ ไม่ไหลเยิ้มเลอเทอะ 
+                        มีกุญแจขันล๊อคตัวกระบอกกับแป้นยึดทั้งแบบบรรจุหัวเดียว 2หัว 3หัว 
+                        หมาะสมในห้องน้ำส่วนกลาง-ในห้องพัก
+                    </p>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px; text-align:center;">
+                <a href="<?php echo $url_link_2; ?>">   
+                    <img src='<?php echo $path_image."directmail_2.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <span>
+                    <a href="<?php echo $url_link_2; ?>"> 
+                        <h3>จานวางสบู่</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        ผลิตด้วยวัสดุคุณภาพหลากหลายชนิด รูปแบบคลาสสิค ขนาดเป็นที่นิยมนำสมัยตามแฟชั่น ทั้งวัสดุสีทึบและสีใส 
+                        ออกแบบให้เข้ากับการวางสบู่ก้อนได้อย่างลงตัว ดูดี มีเสน่ห์ สวยงาม
+                    </p>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px; text-align:center;">
+                <a href="<?php echo $url_link_3; ?>"> 
+                    <img src='<?php echo $path_image."directmail_3.jpg"; ?>' style="max-width:100%;">
+                </a>
+                <span>
+                    <a href="<?php echo $url_link_3; ?>"> 
+                        <h3>แผ่นรองกันลื่น</h3>
+                    </a>
+                    <p class="p_descrpition">
+                        วัสดุยางอย่างดี ด้านล่างมีปุ่มสุญญากาศเกาะพื้นกันลื่นไถล หลากหลายแบบ พร้อมปุ่มนวดเท้า (Footzy Mat) 
+                        เพื่อสุขภาพ ลดอาการเมื่อยล้า มีทั้งแบบมี-ไม่มีรูระบายน้ำ ทำความสะอาดง่ายกันเชื้อรา เหมาะสำหรับปูกันลื่นในห้องน้ำ และในอ่างอาบน้ำ
+                    </p>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#2959c2; padding-top:0px; text-align:center; line-height:30px;">
+			<div class="my-row" style="padding-bottom:0px;">
+                <h2 style="color:#FFFFFF">Contact : 083-025-8992</h2>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="my-row" style="background-color:#FFFFFF; padding-top:20px; text-align:center;">
+			<div class="my-row" style="padding-bottom:20px;">
+                <table style="width:90%; margin-right: auto; margin-left: auto;">
+                    <tr>
+                        <td style="text-align:right;"><strong>website:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.pmintermart.com/">https://www.pmintermart.com/</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Lazada:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://bit.ly/3l0wbz2">https://bit.ly/3l0wbz2</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Shopee:</strong></td>
+                        <td style="text-align:left;" >
+                            <a href="https://shp.ee/zr9df3j">https://shp.ee/zr9df3j</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Line official:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://page.line.me/pmintermart">https://page.line.me/pmintermart</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Facebook:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.facebook.com/PM-INTERMART-106806074049943">
+                                https://www.facebook.com/PM-INTERMART-106806074049943</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align:right;"><strong>Instragram:</strong></td>
+                        <td style="text-align:left;">
+                            <a href="https://www.instagram.com/pmintermartofficial/">
+                                https://www.instagram.com/pmintermartofficial/
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
