dfc012f48e126ff76eec6633ed291f8807bcaf99
mroat888 <mroat07@gmail.com>
2022-06-28 22:38:18 +0700

    ีupdate

--- include_footer.php
+++ include_footer.php
@@ -59,16 +59,6 @@
 
 </script>
 
-<!-- Global site tag (gtag.js) - Google Analytics **ตัวใหม่** -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LDZ9JS1X66"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LDZ9JS1X66');
-</script>
-
 
 <script type="text/javascript" defer>
 
